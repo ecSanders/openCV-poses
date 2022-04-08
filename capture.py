e77eecd56b694ab32a3830e99d69a87ae21276d6
ecSanders <ecsander0@gmail.com>
2022-04-07 18:30:27 -0600

    Move & create

--- capture.py
+++ capture.py
@@ -1,4 +1,30 @@
 import pyrealsense2 as rs
 import cv2
 import numpy as np
+import time
 
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+pipeline.start(config)
+time.sleep(3)
+frames = pipeline.wait_for_frames()
+
+color_frame = frames.get_color_frame()
+color_image = np.asanyarray(color_frame.get_data())
+cv2.imwrite('rect01.png', color_image)
